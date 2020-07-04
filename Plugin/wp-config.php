22002700e26e1b58d5b44ef4aafc74d41681af4d
MD-Asaduzzaman-Muhid <asaduzzaman.muhid@gmail.com>
2020-07-04 20:05:04 +0600

    update plugin

--- Plugin/wp-config.php
+++ Plugin/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress_play' );
+define( 'DB_NAME', 'wp-plugin' );
 
 /** MySQL database username */
-define( 'DB_USER', 'asad' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'asad' );
+define( 'DB_PASSWORD', 'Muhid2277' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
---
+++ Plugin/wp-content/plugins/io-field/io-field.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *  @package io-field
+ */
+/*
+Plugin Name: io-field
+Plugin URI: http://iosoup.com
+Description: This is a custom field plugin.
+Version: 1.0.0
+Author: Asaduzzaman
+Author URI: http://iosoup.com
+License: GPLv2 or later
+Text Domain: io-field
+
+
+*/
